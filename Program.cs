5bd34d156359db61178d4760f0e0270dfd8bd1e8
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 11:46:21 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boucle4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            for(count =10; count >=0/* possibilité de mettre un chiffre negatif*/; count--)
+            {
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
